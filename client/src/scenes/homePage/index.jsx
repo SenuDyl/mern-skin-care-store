01718664894cbfcdfb9117019eac9a456e0ba83c
Senuri Mallikarachchi <senurimallikarachchi@gmail.com>
2025-01-05 14:45:27 +0530

    Add Client files

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import Navbar from '../Navbar'
+
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default HomePage
