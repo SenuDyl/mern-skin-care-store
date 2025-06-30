0986cc256aa0730bdb5238955a7b0cf7ce59dbe3
Senuri Mallikarachchi <senurimallikarachchi@gmail.com>
2025-07-01 02:46:40 +0530

    Create a new react project for frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { useMemo } from 'react'; //improve performance by memoizing the result of a function and only recalculating it when necessary
 import { useSelector } from 'react-redux';
-import HomePage from './scenes/homePage';
+import HomePage from './scenes/HomePage';
+import ProductPage from './scenes/ProductPage';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { themeSettings } from './theme';
+import SampleForm from './scenes/SampleForm';
+import CategoryPage from './scenes/CategoryPage';
+import CartPage from './scenes/CartPage';
+import CheckoutPage from './scenes/CheckoutPage';
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings("light")), ["light"]);
@@ -15,6 +20,11 @@ function App() {
         <Routes>
 
           <Route path="/home" element={<HomePage />} />
+          <Route path="/products/:id" element={<ProductPage />} />
+          <Route path="/category" element={<CategoryPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/" element={<SampleForm />} />
 
         </Routes>
       </ThemeProvider>
