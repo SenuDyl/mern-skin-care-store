f2e3e1782f280f07aa68ba96a8f5b02c6b5608d6
Senuri Mallikarachchi <senurimallikarachchi@gmail.com>
2025-07-11 01:18:49 +0530

    Add new logo and name

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,40 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { useMemo } from 'react'; //improve performance by memoizing the result of a function and only recalculating it when necessary
-import { useSelector } from 'react-redux';
-import HomePage from './scenes/HomePage';
-import ProductPage from './scenes/ProductPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useMemo } from 'react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { themeSettings } from './theme';
-import SampleForm from './scenes/SampleForm';
+import HomePage from './scenes/HomePage';
+import ProductPage from './scenes/ProductPage';
 import CategoryPage from './scenes/CategoryPage';
 import CartPage from './scenes/CartPage';
 import CheckoutPage from './scenes/CheckoutPage';
+import LoginPage from './scenes/LoginPage';
+import RegisterPage from './scenes/RegisterPage';
+import { AuthProvider } from './hooks/AuthContext'; // Make sure this is imported
+import VerifyPage from './scenes/VerifyPage';
+import { CartProvider } from './hooks/CartContext';
 
 function App() {
-  const theme = useMemo(() => createTheme(themeSettings("light")), ["light"]);
+  const theme = useMemo(() => createTheme(themeSettings("light")), []);
+
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Routes>
-
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/products/:id" element={<ProductPage />} />
-          <Route path="/category" element={<CategoryPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/" element={<SampleForm />} />
-
-        </Routes>
+        <AuthProvider>
+          <CartProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/products/:id" element={<ProductPage />} />
+            <Route path="/category" element={<CategoryPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/verify" element={<VerifyPage />} />
+          </Routes>
+          </CartProvider>
+        </AuthProvider>
       </ThemeProvider>
     </BrowserRouter>
   );
